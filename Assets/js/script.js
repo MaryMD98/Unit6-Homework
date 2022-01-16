21f040b1c1f7ea8ebe16cf673e7f966373342ab6
Maribel Montes <maribel.doris@gmail.com>
2022-01-16 16:16:57 -0600

    added console log to gather info for day 2

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,43 +2,41 @@ var openWeatherAPI = "7624e64d3821f1c4e9cbd917e59c3d78";
 var placeTOsearch = 'London,uk';
 var DataResponse;
 
-// example
-// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=7624e64d3821f1c4e9cbd917e59c3d78
 
-function getcurrentWeatehr(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + placeTOsearch + '&APPID=' + openWeatherAPI)
+// function getcurrentWeatehr(){
+//     fetch('http://api.openweathermap.org/data/2.5/weather?q=' + placeTOsearch + '&APPID=' + openWeatherAPI)
     
-      .then(function(response){
-        if (response.ok){
+//       .then(function(response){
+//         if (response.ok){
 
-          response.json()
-            .then(function(data) {
-                console.log("data in getcurrentWeatehr data ")
-                console.log(data);
-              // save data to global parameter to use in other functions
-              DataResponse = data;
+//           response.json()
+//             .then(function(data) {
+//                 console.log("data in getcurrentWeatehr data ")
+//                 console.log(data);
+//               // save data to global parameter to use in other functions
+//               DataResponse = data;
 
-              console.log("data sucessfuls accessed");
-              console.log("icon " + DataResponse.weather[0].icon);
-              var temp = DataResponse.main.temp;
-              temp = temp - 273.15;
-              temp = temp * 1.8;
-              var tempF = temp + 32;
-              tempF = Math.round(tempF);
-              console.log("Temperature " + tempF);
-              console.log("humidity " + DataResponse.main.humidity);
-              console.log("wind Speed " + DataResponse.wind.speed);
-              console.log("latitude " + DataResponse.coord.lat);
-              console.log("longitude " + DataResponse.coord.lon);
+//               console.log("data sucessfuls accessed");
+//               console.log("icon " + DataResponse.weather[0].icon);
+//               var temp = DataResponse.main.temp;
+//               temp = temp - 273.15;
+//               temp = temp * 1.8;
+//               var tempF = temp + 32;
+//               tempF = Math.round(tempF);
+//               console.log("Temperature " + tempF);
+//               console.log("humidity " + DataResponse.main.humidity);
+//               console.log("wind Speed " + DataResponse.wind.speed);
+//               console.log("latitude " + DataResponse.coord.lat);
+//               console.log("longitude " + DataResponse.coord.lon);
               
-          });
-        } else {
-          console.log("error");
-        }
-      })
-}
+//           });
+//         } else {
+//           console.log("error");
+//         }
+//       })
+// }
 
-getcurrentWeatehr();
+// getcurrentWeatehr();
 
 var latitude = '51.5085';
 var longitude = '-0.1257';
@@ -65,11 +63,21 @@ function getONECALLWeatehr(){
               console.log("wind Speed " + DataResponse.current.wind_speed);
               console.log("UV INdex " + DataResponse.current.uvi);
 
-              console.log("data of day + 1");
-              onsole.log("data of day + 2");
-              onsole.log("data of day + 3");
-              onsole.log("data of day + 4");
-              onsole.log("data of day + 5");
+              console.log("data of day + 1*****");
+              console.log("icon " + DataResponse.daily[1].weather[0].icon);
+              console.log("temp max "+ DataResponse.daily[1].temp.max);
+              console.log("temp min "+ DataResponse.daily[1].temp.min);
+              console.log("wind speed "+ DataResponse.daily[1].wind_speed);
+              console.log("Humidity "+ DataResponse.daily[1].humidity);
+              console.log("data of day + 2 *********");
+              console.log("icon " + DataResponse.daily[2].weather[0].icon);
+              console.log("temp max "+ DataResponse.daily[2].temp.max);
+              console.log("temp min "+ DataResponse.daily[2].temp.min);
+              console.log("wind speed "+ DataResponse.daily[2].wind_speed);
+              console.log("Humidity "+ DataResponse.daily[2].humidity);
+              console.log("data of day + 3 *********");
+              console.log("data of day + 4");
+              console.log("data of day + 5");
              
               
           });
